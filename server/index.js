57abef05267ed635259ce625c00f55e0c904ff67
sbssai123 <sbs.sai@gmail.com>
2020-10-02 15:50:31 -0400

    updating scripts

--- server/index.js
+++ server/index.js
@@ -1,15 +1,20 @@
-import Websocket from 'ws'
+import Websocket from 'ws';
+import express from 'express';
 
 const PORT_NUMBER = 8080
 
-const websocketServer = new Websocket.Server({port: PORT_NUMBER})
+const server = express()
+.use(express.static(__dirname + '/../build'))
+.listen(PORT_NUMBER, () => console.log(`Listening on ${PORT_NUMBER}`));
+
+const wss = new Websocket.Server({server});
 
 // When there is an established connection
-websocketServer.on('connection', (wsClient) => {
+wss.on('connection', (wsClient) => {
     // When a message is received from a connected client
     wsClient.on('message', (message) => {
         // broadcast incoming message to all clients connected to the server
-        websocketServer.clients.forEach(client => {
+        wss.clients.forEach(client => {
             // Only send to the client if its not the client sending the message
             if (client !== wsClient && client.readyState === Websocket.OPEN) {
                 client.send(message);
